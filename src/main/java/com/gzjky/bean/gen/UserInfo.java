ce45840a4149825c47321ad69fc4b5f200614645
yuting0787 <626808708@qq.com>
2016-01-08 18:02:10 +0800

    做成

---
+++ src/main/java/com/gzjky/bean/gen/UserInfo.java
@@ -0,0 +1,235 @@
+package com.gzjky.bean.gen;
+
+import org.joda.time.DateTime;
+
+public class UserInfo {
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private Boolean enabled;
+
+    private String gender;
+
+    private String chinesename;
+
+    private String englishname;
+
+    private String photo;
+
+    private String qq;
+
+    private String companyemail;
+
+    private String officephone;
+
+    private String officephoneext;
+
+    private String homephone;
+
+    private String cellphone;
+
+    private String address;
+
+    private String remark;
+
+    private String identitycard;
+
+    private DateTime birthday;
+
+    private DateTime takeofficetime;
+
+    private DateTime lastlogintime;
+
+    private DateTime createtime;
+
+    private Integer deptid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getChinesename() {
+        return chinesename;
+    }
+
+    public void setChinesename(String chinesename) {
+        this.chinesename = chinesename;
+    }
+
+    public String getEnglishname() {
+        return englishname;
+    }
+
+    public void setEnglishname(String englishname) {
+        this.englishname = englishname;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getQq() {
+        return qq;
+    }
+
+    public void setQq(String qq) {
+        this.qq = qq;
+    }
+
+    public String getCompanyemail() {
+        return companyemail;
+    }
+
+    public void setCompanyemail(String companyemail) {
+        this.companyemail = companyemail;
+    }
+
+    public String getOfficephone() {
+        return officephone;
+    }
+
+    public void setOfficephone(String officephone) {
+        this.officephone = officephone;
+    }
+
+    public String getOfficephoneext() {
+        return officephoneext;
+    }
+
+    public void setOfficephoneext(String officephoneext) {
+        this.officephoneext = officephoneext;
+    }
+
+    public String getHomephone() {
+        return homephone;
+    }
+
+    public void setHomephone(String homephone) {
+        this.homephone = homephone;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getIdentitycard() {
+        return identitycard;
+    }
+
+    public void setIdentitycard(String identitycard) {
+        this.identitycard = identitycard;
+    }
+
+    public DateTime getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(DateTime birthday) {
+        this.birthday = birthday;
+    }
+
+    public DateTime getTakeofficetime() {
+        return takeofficetime;
+    }
+
+    public void setTakeofficetime(DateTime takeofficetime) {
+        this.takeofficetime = takeofficetime;
+    }
+
+    public DateTime getLastlogintime() {
+        return lastlogintime;
+    }
+
+    public void setLastlogintime(DateTime lastlogintime) {
+        this.lastlogintime = lastlogintime;
+    }
+
+    public DateTime getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(DateTime createtime) {
+        this.createtime = createtime;
+    }
+
+    public Integer getDeptid() {
+        return deptid;
+    }
+
+    public void setDeptid(Integer deptid) {
+        this.deptid = deptid;
+    }
+}
