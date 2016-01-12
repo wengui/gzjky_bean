d2e3c75ac2d7cd7cde210500ea2ba64c934f9caa
yuting0787 <626808708@qq.com>
2016-01-12 18:25:14 +0800

    做成

---
+++ src/main/java/com/gzjky/bean/extend/PatientInfoOutputBean.java
@@ -0,0 +1,555 @@
+package com.gzjky.bean.extend;
+
+
+public class PatientInfoOutputBean {
+	
+	/**
+	 * 患者ID
+	 */
+    private String id;
+
+    /**
+     * 区域
+     */
+    private String area;
+
+    /**
+     * 健康证
+     */
+    private String healthcardnumber;
+
+    /**
+     * 身份证号
+     */
+    private String idcardnumber;
+
+    /**
+     * 患者姓名
+     */
+    private String patientname;
+
+    /**
+     * 患者性别
+     */
+    private String patientsex;
+
+    /**
+     * 患者生日
+     */
+    private String patientbirthday;
+
+    /**
+     * 合作商ID
+     */
+    private String partnersid;
+
+    /**
+     * 证件类型
+     */
+    private String cardtype;
+
+    /**
+     * 证件号码
+     */
+    private String cardnum;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 患者电话号
+     */
+    private String patientphone;
+
+    /**
+     * 婚姻情况
+     */
+    private String marriagestatus;
+
+    /**
+     * 民族
+     */
+    private String patientnational;
+
+    /**
+     * 是否残疾
+     */
+    private String isdisability;
+
+    /**
+     * 残疾证号
+     */
+    private String disabilitynum;
+
+    /**
+     * 残疾等级
+     */
+    private String disabilitylevel;
+
+    /**
+     * 医保形式
+     */
+    private String healthtype;
+
+    /**
+     * 职业类别
+     */
+    private String professionaltype;
+
+    /**
+     * 身高
+     */
+    private String height;
+
+    /**
+     * 体重
+     */
+    private String weight;
+
+    /**
+     * 固话
+     */
+    private String telephone;
+
+    /**
+     * 工作年限
+     */
+    private String workyears;
+
+    private String issoldier;
+
+    private String militarytime;
+
+    private String positioncode;
+
+    private String typecode;
+
+    private String political;
+
+    private String joinparty;
+
+    private String countries;
+
+    private String province;
+
+    private String city;
+
+    private String homeaddress;
+
+    private String nativeplace;
+
+    private String worktime;
+
+    private String education;
+
+    private String householdtype;
+
+    private String membertype;
+
+    private String annualincome;
+
+    private String regdatetime;
+
+    private String ischeck;
+
+    private String isdelete;
+
+    private String patientimage;
+
+    private String disabilityinfo;
+
+    private String allergy;
+
+    private String familygenetichistory;
+
+    private String companyname;
+
+    private String companyaddress;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	public String getHealthcardnumber() {
+		return healthcardnumber;
+	}
+
+	public void setHealthcardnumber(String healthcardnumber) {
+		this.healthcardnumber = healthcardnumber;
+	}
+
+	public String getIdcardnumber() {
+		return idcardnumber;
+	}
+
+	public void setIdcardnumber(String idcardnumber) {
+		this.idcardnumber = idcardnumber;
+	}
+
+	public String getPatientname() {
+		return patientname;
+	}
+
+	public void setPatientname(String patientname) {
+		this.patientname = patientname;
+	}
+
+	public String getPatientsex() {
+		return patientsex;
+	}
+
+	public void setPatientsex(String patientsex) {
+		this.patientsex = patientsex;
+	}
+
+	public String getPatientbirthday() {
+		return patientbirthday;
+	}
+
+	public void setPatientbirthday(String patientbirthday) {
+		this.patientbirthday = patientbirthday;
+	}
+
+	public String getPartnersid() {
+		return partnersid;
+	}
+
+	public void setPartnersid(String partnersid) {
+		this.partnersid = partnersid;
+	}
+
+	public String getCardtype() {
+		return cardtype;
+	}
+
+	public void setCardtype(String cardtype) {
+		this.cardtype = cardtype;
+	}
+
+	public String getCardnum() {
+		return cardnum;
+	}
+
+	public void setCardnum(String cardnum) {
+		this.cardnum = cardnum;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPatientphone() {
+		return patientphone;
+	}
+
+	public void setPatientphone(String patientphone) {
+		this.patientphone = patientphone;
+	}
+
+	public String getMarriagestatus() {
+		return marriagestatus;
+	}
+
+	public void setMarriagestatus(String marriagestatus) {
+		this.marriagestatus = marriagestatus;
+	}
+
+	public String getPatientnational() {
+		return patientnational;
+	}
+
+	public void setPatientnational(String patientnational) {
+		this.patientnational = patientnational;
+	}
+
+	public String getIsdisability() {
+		return isdisability;
+	}
+
+	public void setIsdisability(String isdisability) {
+		this.isdisability = isdisability;
+	}
+
+	public String getDisabilitynum() {
+		return disabilitynum;
+	}
+
+	public void setDisabilitynum(String disabilitynum) {
+		this.disabilitynum = disabilitynum;
+	}
+
+	public String getDisabilitylevel() {
+		return disabilitylevel;
+	}
+
+	public void setDisabilitylevel(String disabilitylevel) {
+		this.disabilitylevel = disabilitylevel;
+	}
+
+	public String getHealthtype() {
+		return healthtype;
+	}
+
+	public void setHealthtype(String healthtype) {
+		this.healthtype = healthtype;
+	}
+
+	public String getProfessionaltype() {
+		return professionaltype;
+	}
+
+	public void setProfessionaltype(String professionaltype) {
+		this.professionaltype = professionaltype;
+	}
+
+	public String getHeight() {
+		return height;
+	}
+
+	public void setHeight(String height) {
+		this.height = height;
+	}
+
+	public String getWeight() {
+		return weight;
+	}
+
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getWorkyears() {
+		return workyears;
+	}
+
+	public void setWorkyears(String workyears) {
+		this.workyears = workyears;
+	}
+
+	public String getIssoldier() {
+		return issoldier;
+	}
+
+	public void setIssoldier(String issoldier) {
+		this.issoldier = issoldier;
+	}
+
+	public String getMilitarytime() {
+		return militarytime;
+	}
+
+	public void setMilitarytime(String militarytime) {
+		this.militarytime = militarytime;
+	}
+
+	public String getPositioncode() {
+		return positioncode;
+	}
+
+	public void setPositioncode(String positioncode) {
+		this.positioncode = positioncode;
+	}
+
+	public String getTypecode() {
+		return typecode;
+	}
+
+	public void setTypecode(String typecode) {
+		this.typecode = typecode;
+	}
+
+	public String getPolitical() {
+		return political;
+	}
+
+	public void setPolitical(String political) {
+		this.political = political;
+	}
+
+	public String getJoinparty() {
+		return joinparty;
+	}
+
+	public void setJoinparty(String joinparty) {
+		this.joinparty = joinparty;
+	}
+
+	public String getCountries() {
+		return countries;
+	}
+
+	public void setCountries(String countries) {
+		this.countries = countries;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getHomeaddress() {
+		return homeaddress;
+	}
+
+	public void setHomeaddress(String homeaddress) {
+		this.homeaddress = homeaddress;
+	}
+
+	public String getNativeplace() {
+		return nativeplace;
+	}
+
+	public void setNativeplace(String nativeplace) {
+		this.nativeplace = nativeplace;
+	}
+
+	public String getWorktime() {
+		return worktime;
+	}
+
+	public void setWorktime(String worktime) {
+		this.worktime = worktime;
+	}
+
+	public String getEducation() {
+		return education;
+	}
+
+	public void setEducation(String education) {
+		this.education = education;
+	}
+
+	public String getHouseholdtype() {
+		return householdtype;
+	}
+
+	public void setHouseholdtype(String householdtype) {
+		this.householdtype = householdtype;
+	}
+
+	public String getMembertype() {
+		return membertype;
+	}
+
+	public void setMembertype(String membertype) {
+		this.membertype = membertype;
+	}
+
+	public String getAnnualincome() {
+		return annualincome;
+	}
+
+	public void setAnnualincome(String annualincome) {
+		this.annualincome = annualincome;
+	}
+
+	public String getRegdatetime() {
+		return regdatetime;
+	}
+
+	public void setRegdatetime(String regdatetime) {
+		this.regdatetime = regdatetime;
+	}
+
+	public String getIscheck() {
+		return ischeck;
+	}
+
+	public void setIscheck(String ischeck) {
+		this.ischeck = ischeck;
+	}
+
+	public String getIsdelete() {
+		return isdelete;
+	}
+
+	public void setIsdelete(String isdelete) {
+		this.isdelete = isdelete;
+	}
+
+	public String getPatientimage() {
+		return patientimage;
+	}
+
+	public void setPatientimage(String patientimage) {
+		this.patientimage = patientimage;
+	}
+
+	public String getDisabilityinfo() {
+		return disabilityinfo;
+	}
+
+	public void setDisabilityinfo(String disabilityinfo) {
+		this.disabilityinfo = disabilityinfo;
+	}
+
+	public String getAllergy() {
+		return allergy;
+	}
+
+	public void setAllergy(String allergy) {
+		this.allergy = allergy;
+	}
+
+	public String getFamilygenetichistory() {
+		return familygenetichistory;
+	}
+
+	public void setFamilygenetichistory(String familygenetichistory) {
+		this.familygenetichistory = familygenetichistory;
+	}
+
+	public String getCompanyname() {
+		return companyname;
+	}
+
+	public void setCompanyname(String companyname) {
+		this.companyname = companyname;
+	}
+
+	public String getCompanyaddress() {
+		return companyaddress;
+	}
+
+	public void setCompanyaddress(String companyaddress) {
+		this.companyaddress = companyaddress;
+	}
+
+}
