67ebc7e84255474a0f2d50b8cc7499987b62425f
yuting0787 <626808708@qq.com>
2016-01-14 14:58:13 +0800

    做成

---
+++ src/main/java/com/gzjky/bean/extend/TreeNodesBean.java
@@ -0,0 +1,45 @@
+package com.gzjky.bean.extend;
+
+public class TreeNodesBean {
+	
+	private String id;
+	
+	private String pId;
+	
+	private String name;
+	
+	private String type;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getpId() {
+		return pId;
+	}
+
+	public void setpId(String pId) {
+		this.pId = pId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
