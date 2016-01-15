74a9eb24ae953f06ba7f1cd2e754aa30d826b324
yuting0787 <626808708@qq.com>
2016-01-15 11:46:55 +0800

    并发症bean做成

---
+++ src/main/java/com/gzjky/bean/gen/PatientNowComplicationsChecked.java
@@ -0,0 +1,33 @@
+package com.gzjky.bean.gen;
+
+public class PatientNowComplicationsChecked {
+    private Integer id;
+
+    private Integer patientid;
+
+    private String diseaseidvalue;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPatientid() {
+        return patientid;
+    }
+
+    public void setPatientid(Integer patientid) {
+        this.patientid = patientid;
+    }
+
+    public String getDiseaseidvalue() {
+        return diseaseidvalue;
+    }
+
+    public void setDiseaseidvalue(String diseaseidvalue) {
+        this.diseaseidvalue = diseaseidvalue;
+    }
+}
