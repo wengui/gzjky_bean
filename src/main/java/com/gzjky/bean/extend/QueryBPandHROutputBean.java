1ef41f13108605fa7f372cb9b5c5fcbb17611b99
yuting0787 <yuting0787@163.com>
2016-01-19 15:25:46 +0800

    主页健康管理

---
+++ src/main/java/com/gzjky/bean/extend/QueryBPandHROutputBean.java
@@ -0,0 +1,148 @@
+package com.gzjky.bean.extend;
+
+public class QueryBPandHROutputBean {
+	
+	/**
+	 * 最近一次收缩压
+	 */
+	public String shrink;
+	
+	/**
+	 * 最近一次舒张压
+	 */
+	public String diastole;
+	
+	/**
+	 * 最近一次测压时间
+	 */
+	public String bloodTakeTime;
+	
+	/**
+	 * 最近一次血压警告收缩压
+	 */
+	public String shrinkAlert;
+	
+	/**
+	 * 最近一次血压警告舒张压
+	 */
+	public String diastoleAlert;
+	
+	/**
+	 * 最近一次血压警告测压时间
+	 */
+	public String bloodTakeTimeAlert;
+	
+	/**
+	 * 最近一次脉率脉率
+	 */
+	public String pulseRate;
+	
+	/**
+	 * 最近一次心率
+	 */
+	public String heartRate;
+	
+	/**
+	 * 最近一次心电测试时间
+	 */
+	public String heartTakeTime;
+	
+	/**
+	 * 最近一次心率警告
+	 */
+	public String heartRateAlert;
+
+	/**
+	 * 最近一次心率警告测试时间
+	 */
+	public String heartTakeTimeAlert;
+
+	public String getShrink() {
+		return shrink;
+	}
+
+	public void setShrink(String shrink) {
+		this.shrink = shrink;
+	}
+
+	public String getDiastole() {
+		return diastole;
+	}
+
+	public void setDiastole(String diastole) {
+		this.diastole = diastole;
+	}
+
+	public String getBloodTakeTime() {
+		return bloodTakeTime;
+	}
+
+	public void setBloodTakeTime(String bloodTakeTime) {
+		this.bloodTakeTime = bloodTakeTime;
+	}
+
+	public String getShrinkAlert() {
+		return shrinkAlert;
+	}
+
+	public void setShrinkAlert(String shrinkAlert) {
+		this.shrinkAlert = shrinkAlert;
+	}
+
+	public String getDiastoleAlert() {
+		return diastoleAlert;
+	}
+
+	public void setDiastoleAlert(String diastoleAlert) {
+		this.diastoleAlert = diastoleAlert;
+	}
+
+	public String getBloodTakeTimeAlert() {
+		return bloodTakeTimeAlert;
+	}
+
+	public void setBloodTakeTimeAlert(String bloodTakeTimeAlert) {
+		this.bloodTakeTimeAlert = bloodTakeTimeAlert;
+	}
+
+	public String getPulseRate() {
+		return pulseRate;
+	}
+
+	public void setPulseRate(String pulseRate) {
+		this.pulseRate = pulseRate;
+	}
+
+	public String getHeartRate() {
+		return heartRate;
+	}
+
+	public void setHeartRate(String heartRate) {
+		this.heartRate = heartRate;
+	}
+
+	public String getHeartTakeTime() {
+		return heartTakeTime;
+	}
+
+	public void setHeartTakeTime(String heartTakeTime) {
+		this.heartTakeTime = heartTakeTime;
+	}
+
+	public String getHeartRateAlert() {
+		return heartRateAlert;
+	}
+
+	public void setHeartRateAlert(String heartRateAlert) {
+		this.heartRateAlert = heartRateAlert;
+	}
+
+	public String getHeartTakeTimeAlert() {
+		return heartTakeTimeAlert;
+	}
+
+	public void setHeartTakeTimeAlert(String heartTakeTimeAlert) {
+		this.heartTakeTimeAlert = heartTakeTimeAlert;
+	}
+
+}
---
+++ src/main/java/com/gzjky/bean/extend/QueryDoctorAdviceOutputBean.java
@@ -0,0 +1,165 @@
+package com.gzjky.bean.extend;
+
+public class QueryDoctorAdviceOutputBean {
+	
+	/**
+	 * 创建时间
+	 */
+	public String createDate;
+	
+	/**
+	 * 测压目标舒张压
+	 */
+	public String targetDpBottom;
+	
+	/**
+	 * 测压目标舒张压
+	 */
+	public String targetDpTop;
+	
+	/**
+	 *测压目标收缩压
+	 */
+	public String targetSpBottom;
+	
+	/**
+	 * 测压目标收缩压
+	 */
+	public String targetSpTop;
+	
+	/**
+	 * 开始时间
+	 */
+	public String startTime;
+	
+	/**
+	 * 频率
+	 */
+	public String pl;
+	
+	/**
+	 * 星期
+	 */
+	public String xq;
+
+	/**
+	 * 时间
+	 */
+	public String hours;
+
+	/**
+	 * 保健建议
+	 */
+	public String suggestion;
+	/**
+	 * 药名
+	 */
+	public String name;
+	/**
+	 * 计量
+	 */
+	public String jl;	
+	/**
+	 * 疗程
+	 */
+	public String lc;
+	
+	/**
+	 * 饭前饭后
+	 */
+	public String fqfh;
+	/**
+	 * 具体时间
+	 */
+	public String jtsj;
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getTargetDpBottom() {
+		return targetDpBottom;
+	}
+	public void setTargetDpBottom(String targetDpBottom) {
+		this.targetDpBottom = targetDpBottom;
+	}
+	public String getTargetDpTop() {
+		return targetDpTop;
+	}
+	public void setTargetDpTop(String targetDpTop) {
+		this.targetDpTop = targetDpTop;
+	}
+	public String getTargetSpBottom() {
+		return targetSpBottom;
+	}
+	public void setTargetSpBottom(String targetSpBottom) {
+		this.targetSpBottom = targetSpBottom;
+	}
+	public String getTargetSpTop() {
+		return targetSpTop;
+	}
+	public void setTargetSpTop(String targetSpTop) {
+		this.targetSpTop = targetSpTop;
+	}
+	public String getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+	public String getPl() {
+		return pl;
+	}
+	public void setPl(String pl) {
+		this.pl = pl;
+	}
+	public String getXq() {
+		return xq;
+	}
+	public void setXq(String xq) {
+		this.xq = xq;
+	}
+	public String getHours() {
+		return hours;
+	}
+	public void setHours(String hours) {
+		this.hours = hours;
+	}
+	public String getSuggestion() {
+		return suggestion;
+	}
+	public void setSuggestion(String suggestion) {
+		this.suggestion = suggestion;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getJl() {
+		return jl;
+	}
+	public void setJl(String jl) {
+		this.jl = jl;
+	}
+	public String getLc() {
+		return lc;
+	}
+	public void setLc(String lc) {
+		this.lc = lc;
+	}
+	public String getFqfh() {
+		return fqfh;
+	}
+	public void setFqfh(String fqfh) {
+		this.fqfh = fqfh;
+	}
+	public String getJtsj() {
+		return jtsj;
+	}
+	public void setJtsj(String jtsj) {
+		this.jtsj = jtsj;
+	}
+}
