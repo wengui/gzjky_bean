fa82e5b8760d6b2a9133bb3d109536014e69b24c
zmj <zmj@zmj-PC>
2016-01-19 15:55:17 +0800

    2016/1/19 主页设备医生等显示

---
+++ src/main/java/com/gzjky/bean/extend/PatientAndDoctorHospitalBean.java
@@ -0,0 +1,57 @@
+package com.gzjky.bean.extend;
+
+public class PatientAndDoctorHospitalBean {
+	
+	private String patientId;
+	
+	private String dId;
+	
+	private String dName;
+	
+	private String hId;
+	
+	private String hName;
+
+	public String getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(String patientId) {
+		this.patientId = patientId;
+	}
+
+	public String getdId() {
+		return dId;
+	}
+
+	public void setdId(String dId) {
+		this.dId = dId;
+	}
+
+	public String getdName() {
+		return dName;
+	}
+
+	public void setdName(String dName) {
+		this.dName = dName;
+	}
+
+	public String gethId() {
+		return hId;
+	}
+
+	public void sethId(String hId) {
+		this.hId = hId;
+	}
+
+	public String gethName() {
+		return hName;
+	}
+
+	public void sethName(String hName) {
+		this.hName = hName;
+	}
+
+
+
+}
---
+++ src/main/java/com/gzjky/bean/extend/PatientAndEquipmentBean.java
@@ -0,0 +1,30 @@
+package com.gzjky.bean.extend;
+
+public class PatientAndEquipmentBean {
+	
+	private String equipmentNum;
+	
+	private String deviceVersionName;
+	
+
+	
+	public String getEquipmentNum() {
+		return equipmentNum;
+	}
+
+	public void setEquipmentNum(String equipmentNum) {
+		this.equipmentNum = equipmentNum;
+	}
+
+	public String getDeviceVersionName() {
+		return deviceVersionName;
+	}
+
+	public void setDeviceVersionName(String deviceVersionName) {
+		this.deviceVersionName = deviceVersionName;
+	}
+
+
+
+
+}
