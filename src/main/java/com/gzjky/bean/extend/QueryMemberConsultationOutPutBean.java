f4fce53400caa3249fd809db55567baf2d760048
yuting0787 <yuting0787@163.com>
2016-01-19 16:33:07 +0800

    最新咨询

---
+++ src/main/java/com/gzjky/bean/extend/QueryMemberConsultationOutPutBean.java
@@ -0,0 +1,133 @@
+package com.gzjky.bean.extend;
+
+public class QueryMemberConsultationOutPutBean {
+    private Integer id;
+
+    private String symptom;
+
+    private String consultingcontent;
+
+    private String consultingtime;
+
+    private String result;
+
+    private String resulttime;
+
+    private String state;
+
+    private String createdon;
+
+    private String savedon;
+
+    private Boolean issendmessage;
+
+    private String doctorloginname;
+
+    private Integer patientid;
+
+    private String symptomname;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSymptom() {
+        return symptom;
+    }
+
+    public void setSymptom(String symptom) {
+        this.symptom = symptom;
+    }
+
+    public String getConsultingcontent() {
+        return consultingcontent;
+    }
+
+    public void setConsultingcontent(String consultingcontent) {
+        this.consultingcontent = consultingcontent;
+    }
+
+    public String getConsultingtime() {
+        return consultingtime;
+    }
+
+    public void setConsultingtime(String consultingtime) {
+        this.consultingtime = consultingtime;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getResulttime() {
+        return resulttime;
+    }
+
+    public void setResulttime(String resulttime) {
+        this.resulttime = resulttime;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCreatedon() {
+        return createdon;
+    }
+
+    public void setCreatedon(String createdon) {
+        this.createdon = createdon;
+    }
+
+    public String getSavedon() {
+        return savedon;
+    }
+
+    public void setSavedon(String savedon) {
+        this.savedon = savedon;
+    }
+
+    public Boolean getIssendmessage() {
+        return issendmessage;
+    }
+
+    public void setIssendmessage(Boolean issendmessage) {
+        this.issendmessage = issendmessage;
+    }
+
+    public String getDoctorloginname() {
+        return doctorloginname;
+    }
+
+    public void setDoctorloginname(String doctorloginname) {
+        this.doctorloginname = doctorloginname;
+    }
+
+    public Integer getPatientid() {
+        return patientid;
+    }
+
+    public void setPatientid(Integer patientid) {
+        this.patientid = patientid;
+    }
+
+    public String getSymptomname() {
+        return symptomname;
+    }
+
+    public void setSymptomname(String symptomname) {
+        this.symptomname = symptomname;
+    }
+}
