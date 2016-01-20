3ea8dfe60c4473c71acde7404ba6e5e12635e82f
yuting0787 <626808708@qq.com>
2016-01-20 13:08:45 +0800

    周报和月报bean做成

---
+++ src/main/java/com/gzjky/bean/gen/WeeklyReports.java
@@ -0,0 +1,376 @@
+package com.gzjky.bean.gen;
+
+import java.math.BigDecimal;
+import org.joda.time.DateTime;
+
+public class WeeklyReports {
+    private Integer id;
+
+    private String starttime;
+
+    private String endtime;
+
+    private Integer goalofsbp;
+
+    private Integer goalofdbp;
+
+    private String bloodlevel;
+
+    private String riskstratification;
+
+    private String cardiovascularriskfactors;
+
+    private String targetorgan;
+
+    private String clinicaldisease;
+
+    private Integer meanbloodpressureofsbp;
+
+    private Integer meanbloodpressureofdbp;
+
+    private BigDecimal sdmean;
+
+    private Integer bloodpressureloadofsbp;
+
+    private Integer bloodpressureloadofdbp;
+
+    private Integer bloodpressureloadofmap;
+
+    private Integer bloodpressureloadofhr;
+
+    private Integer maxsbp;
+
+    private String maxsbptime;
+
+    private Integer minsbp;
+
+    private String minsbptime;
+
+    private Integer maxdbp;
+
+    private String maxdbptime;
+
+    private Integer mindbp;
+
+    private String mindbptime;
+
+    private String completionstatus;
+
+    private String noindication;
+
+    private String summary;
+
+    private String healthadvice;
+
+    private String analysisresult;
+
+    private String doctorhealthadvice;
+
+    private Boolean issendmessage;
+
+    private String doctorloginname;
+
+    private String status;
+
+    private DateTime createdon;
+
+    private DateTime savedon;
+
+    private Integer patientid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStarttime() {
+        return starttime;
+    }
+
+    public void setStarttime(String starttime) {
+        this.starttime = starttime;
+    }
+
+    public String getEndtime() {
+        return endtime;
+    }
+
+    public void setEndtime(String endtime) {
+        this.endtime = endtime;
+    }
+
+    public Integer getGoalofsbp() {
+        return goalofsbp;
+    }
+
+    public void setGoalofsbp(Integer goalofsbp) {
+        this.goalofsbp = goalofsbp;
+    }
+
+    public Integer getGoalofdbp() {
+        return goalofdbp;
+    }
+
+    public void setGoalofdbp(Integer goalofdbp) {
+        this.goalofdbp = goalofdbp;
+    }
+
+    public String getBloodlevel() {
+        return bloodlevel;
+    }
+
+    public void setBloodlevel(String bloodlevel) {
+        this.bloodlevel = bloodlevel;
+    }
+
+    public String getRiskstratification() {
+        return riskstratification;
+    }
+
+    public void setRiskstratification(String riskstratification) {
+        this.riskstratification = riskstratification;
+    }
+
+    public String getCardiovascularriskfactors() {
+        return cardiovascularriskfactors;
+    }
+
+    public void setCardiovascularriskfactors(String cardiovascularriskfactors) {
+        this.cardiovascularriskfactors = cardiovascularriskfactors;
+    }
+
+    public String getTargetorgan() {
+        return targetorgan;
+    }
+
+    public void setTargetorgan(String targetorgan) {
+        this.targetorgan = targetorgan;
+    }
+
+    public String getClinicaldisease() {
+        return clinicaldisease;
+    }
+
+    public void setClinicaldisease(String clinicaldisease) {
+        this.clinicaldisease = clinicaldisease;
+    }
+
+    public Integer getMeanbloodpressureofsbp() {
+        return meanbloodpressureofsbp;
+    }
+
+    public void setMeanbloodpressureofsbp(Integer meanbloodpressureofsbp) {
+        this.meanbloodpressureofsbp = meanbloodpressureofsbp;
+    }
+
+    public Integer getMeanbloodpressureofdbp() {
+        return meanbloodpressureofdbp;
+    }
+
+    public void setMeanbloodpressureofdbp(Integer meanbloodpressureofdbp) {
+        this.meanbloodpressureofdbp = meanbloodpressureofdbp;
+    }
+
+    public BigDecimal getSdmean() {
+        return sdmean;
+    }
+
+    public void setSdmean(BigDecimal sdmean) {
+        this.sdmean = sdmean;
+    }
+
+    public Integer getBloodpressureloadofsbp() {
+        return bloodpressureloadofsbp;
+    }
+
+    public void setBloodpressureloadofsbp(Integer bloodpressureloadofsbp) {
+        this.bloodpressureloadofsbp = bloodpressureloadofsbp;
+    }
+
+    public Integer getBloodpressureloadofdbp() {
+        return bloodpressureloadofdbp;
+    }
+
+    public void setBloodpressureloadofdbp(Integer bloodpressureloadofdbp) {
+        this.bloodpressureloadofdbp = bloodpressureloadofdbp;
+    }
+
+    public Integer getBloodpressureloadofmap() {
+        return bloodpressureloadofmap;
+    }
+
+    public void setBloodpressureloadofmap(Integer bloodpressureloadofmap) {
+        this.bloodpressureloadofmap = bloodpressureloadofmap;
+    }
+
+    public Integer getBloodpressureloadofhr() {
+        return bloodpressureloadofhr;
+    }
+
+    public void setBloodpressureloadofhr(Integer bloodpressureloadofhr) {
+        this.bloodpressureloadofhr = bloodpressureloadofhr;
+    }
+
+    public Integer getMaxsbp() {
+        return maxsbp;
+    }
+
+    public void setMaxsbp(Integer maxsbp) {
+        this.maxsbp = maxsbp;
+    }
+
+    public String getMaxsbptime() {
+        return maxsbptime;
+    }
+
+    public void setMaxsbptime(String maxsbptime) {
+        this.maxsbptime = maxsbptime;
+    }
+
+    public Integer getMinsbp() {
+        return minsbp;
+    }
+
+    public void setMinsbp(Integer minsbp) {
+        this.minsbp = minsbp;
+    }
+
+    public String getMinsbptime() {
+        return minsbptime;
+    }
+
+    public void setMinsbptime(String minsbptime) {
+        this.minsbptime = minsbptime;
+    }
+
+    public Integer getMaxdbp() {
+        return maxdbp;
+    }
+
+    public void setMaxdbp(Integer maxdbp) {
+        this.maxdbp = maxdbp;
+    }
+
+    public String getMaxdbptime() {
+        return maxdbptime;
+    }
+
+    public void setMaxdbptime(String maxdbptime) {
+        this.maxdbptime = maxdbptime;
+    }
+
+    public Integer getMindbp() {
+        return mindbp;
+    }
+
+    public void setMindbp(Integer mindbp) {
+        this.mindbp = mindbp;
+    }
+
+    public String getMindbptime() {
+        return mindbptime;
+    }
+
+    public void setMindbptime(String mindbptime) {
+        this.mindbptime = mindbptime;
+    }
+
+    public String getCompletionstatus() {
+        return completionstatus;
+    }
+
+    public void setCompletionstatus(String completionstatus) {
+        this.completionstatus = completionstatus;
+    }
+
+    public String getNoindication() {
+        return noindication;
+    }
+
+    public void setNoindication(String noindication) {
+        this.noindication = noindication;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getHealthadvice() {
+        return healthadvice;
+    }
+
+    public void setHealthadvice(String healthadvice) {
+        this.healthadvice = healthadvice;
+    }
+
+    public String getAnalysisresult() {
+        return analysisresult;
+    }
+
+    public void setAnalysisresult(String analysisresult) {
+        this.analysisresult = analysisresult;
+    }
+
+    public String getDoctorhealthadvice() {
+        return doctorhealthadvice;
+    }
+
+    public void setDoctorhealthadvice(String doctorhealthadvice) {
+        this.doctorhealthadvice = doctorhealthadvice;
+    }
+
+    public Boolean getIssendmessage() {
+        return issendmessage;
+    }
+
+    public void setIssendmessage(Boolean issendmessage) {
+        this.issendmessage = issendmessage;
+    }
+
+    public String getDoctorloginname() {
+        return doctorloginname;
+    }
+
+    public void setDoctorloginname(String doctorloginname) {
+        this.doctorloginname = doctorloginname;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public DateTime getCreatedon() {
+        return createdon;
+    }
+
+    public void setCreatedon(DateTime createdon) {
+        this.createdon = createdon;
+    }
+
+    public DateTime getSavedon() {
+        return savedon;
+    }
+
+    public void setSavedon(DateTime savedon) {
+        this.savedon = savedon;
+    }
+
+    public Integer getPatientid() {
+        return patientid;
+    }
+
+    public void setPatientid(Integer patientid) {
+        this.patientid = patientid;
+    }
+}
