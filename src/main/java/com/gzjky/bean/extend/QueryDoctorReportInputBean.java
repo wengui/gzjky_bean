43abb204305422713e74032224a7f77239f48741
yuting0787 <yuting0787@163.com>
2016-01-21 16:45:33 +0800

    医生报告

---
+++ src/main/java/com/gzjky/bean/extend/QueryDoctorReportInputBean.java
@@ -0,0 +1,48 @@
+package com.gzjky.bean.extend;
+
+/**
+ * 血压查询参数入力Bean
+ * @author yuting
+ *
+ */
+public class QueryDoctorReportInputBean extends PageBean {
+
+	/**
+	 * 开始时间
+	 */
+	private String startDate;
+	
+	/**
+	 * 结束时间
+	 */
+	private String endDate;
+	
+	/**
+	 * 患者ID
+	 */
+	private String patientId;
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(String patientId) {
+		this.patientId = patientId;
+	}
+}
