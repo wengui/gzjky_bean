0e0cd552235227efdb3ad795a651f1aa11591cbf
yuting0787 <626808708@qq.com>
2016-01-22 16:26:13 +0800

    头像上传做成

---
+++ src/main/java/com/gzjky/bean/extend/HeadImageBean.java
@@ -0,0 +1,14 @@
+package com.gzjky.bean.extend;
+
+public class HeadImageBean {
+
+	private byte[] headImage;
+
+	public byte[] getHeadImage() {
+		return headImage;
+	}
+
+	public void setHeadImage(byte[] headImage) {
+		this.headImage = headImage;
+	}
+}
---
+++ src/main/java/com/gzjky/bean/extend/PatientimageInputBean.java
@@ -0,0 +1,24 @@
+package com.gzjky.bean.extend;
+
+public class PatientimageInputBean {
+	
+	private int patienId;
+	
+	private byte[] patientImage;
+
+	public int getPatienId() {
+		return patienId;
+	}
+
+	public void setPatienId(int patienId) {
+		this.patienId = patienId;
+	}
+
+	public byte[] getPatientImage() {
+		return patientImage;
+	}
+
+	public void setPatientImage(byte[] patientImage) {
+		this.patientImage = patientImage;
+	}
+}
