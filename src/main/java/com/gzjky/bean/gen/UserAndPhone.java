387624cdf266bf8440f8ec0f4915d3f83a03a3b1
zmj <zmj@zmj-PC>
2016-03-17 17:52:31 +0800

    app登录页面相关

---
+++ src/main/java/com/gzjky/bean/gen/UserAndPhone.java
@@ -0,0 +1,33 @@
+package com.gzjky.bean.gen;
+
+public class UserAndPhone {
+    private Integer id;
+
+    private String username;
+
+    private String userdevice;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserdevice() {
+        return userdevice;
+    }
+
+    public void setUserdevice(String userdevice) {
+        this.userdevice = userdevice;
+    }
+}
