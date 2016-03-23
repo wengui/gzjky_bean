65e1b71b85920f2d6eb5e538159c81d79dc4248b
tom <tom@tom-PC>
2016-03-23 13:16:29 +0800

    血压心电api做成

---
+++ src/main/java/com/gzjky/bean/extend/EtcOutputBean.java
@@ -0,0 +1,18 @@
+package com.gzjky.bean.extend;
+
+public class EtcOutputBean extends ElectrocardioOutputBean {
+
+	/**
+	 * 心电图
+	 */
+	private String imageUrl;
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
+}
