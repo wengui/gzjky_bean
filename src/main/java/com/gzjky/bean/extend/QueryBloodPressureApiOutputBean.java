5274cd50e76339c1bd1ecf6be002819246ab3138
tom <tom@tom-PC>
2016-04-05 15:27:09 +0800

    血压图做成

---
+++ src/main/java/com/gzjky/bean/extend/QueryBloodPressureApiOutputBean.java
@@ -0,0 +1,19 @@
+package com.gzjky.bean.extend;
+
+public class QueryBloodPressureApiOutputBean extends QueryBloodPressureOutputBean{
+	
+	/**
+	 * 收缩压
+	 */
+	public String average;
+
+	public String getAverage() {
+		return average;
+	}
+
+	public void setAverage(String average) {
+		this.average = average;
+	}
+	
+
+}
